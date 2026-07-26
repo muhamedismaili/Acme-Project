b6da21f49ddc782db64dcfc864530a6b4d68b8dc
Muhamed Ismaili <muhamed@Muhameds-MacBook-Pro.local>
2026-07-26 17:40:24 +0200

    added User import

--- auth.ts
+++ auth.ts
@@ -4,6 +4,7 @@ import Credentials from 'next-auth/providers/credentials';
 import { z } from 'zod'
 import postgres from 'postgres';
 import bcrypt from 'bcrypt';
+import type { User } from '@/app/lib/definitions';
 
 const sql = postgres(process.env.POSTGRES_URL!, {ssl: 'require'});
 
